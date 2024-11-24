a3b977d83babf1c2458af66cbae3d8094315045e
11I1 <kaxcontent@gmail.com>
2024-11-24 11:25:08 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -111,7 +111,7 @@ insertCommand("skill2", function(getPlayer)
                 if not getPlayer.Character or not getPlayer.Character.Humanoid or getPlayer.Character.Humanoid.Health <= 0 then
                     break
                 else
-                    getPlayer.Character:SetPrimaryPartCFrame(tool.Handle.CFrame); firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0, task.wait(), firetouchinterest(tool.Handle, part, 0))
+                    getPlayer.Character:SetPrimaryPartCFrame(tool.Handle.CFrame); firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0, task.wait(), firetouchinterest(getPlayer.Character.PrimaryPart, part, 0))
                     if tool.Parent ~= workspace then tool.Parent = workspace end
                 end
             until plr.Character.Humanoid.Health <= 0
