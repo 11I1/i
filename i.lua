4568c57de63650889a42d4cab0e5417065937003
11I1 <kaxcontent@gmail.com>
2024-11-24 12:03:35 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -106,6 +106,7 @@ insertCommand("skill2", function(getPlayer)
 
         local function run()
             plr.Character.Humanoid:EquipTool(tool); wait(1/4)
+            tool.Parent = workspace
 
             repeat
                 if not getPlayer.Character or not getPlayer.Character.Humanoid or getPlayer.Character.Humanoid.Health <= 0 then
@@ -113,7 +114,6 @@ insertCommand("skill2", function(getPlayer)
                 else
                     plr.Character:SetPrimaryPartCFrame(part.CFrame); getPlayer.Character:SetPrimaryPartCFrame(tool.Handle.CFrame)
                     firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0, task.wait(), firetouchinterest(getPlayer.Character.PrimaryPart, part, 0))
-                    if tool.Parent ~= workspace then tool.Parent = workspace end
                 end
             until plr.Character.Humanoid.Health <= 0
         end task.spawn(function() pcall(run) end)
