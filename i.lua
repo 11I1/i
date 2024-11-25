ca738f69aad1cc3713aeb115a09e3bf282c22970
11I1 <kaxcontent@gmail.com>
2024-11-25 12:18:59 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -146,10 +146,12 @@ insertCommand("svoid", function(getPlayer)
 
             repeat task.wait()
                 if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
-                plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight + 5, 0)); getPlayer.Character:PivotTo(tool.Handle.CFrame)
+                getPlayer.Character:PivotTo(tool.Handle.CFrame)
                 firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0)
                 if tool.Parent ~= workspace then tool.Parent = workspace end
             until getPlayer.Character:FindFirstChild("Sitting")
+
+            plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight + 10, 0))
         end task.spawn(function() pcall(run, os.time()) end)
 
         local clock = os.time()
