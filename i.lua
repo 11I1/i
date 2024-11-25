94d3bcfb36e6446e507804cc01a1d96a021438bd
11I1 <kaxcontent@gmail.com>
2024-11-24 22:47:44 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -52,10 +52,10 @@ insertCommand("skill", function(getPlayer)
         for i, v in next, workspace["Police Station"]:GetChildren() do if v:IsA("BasePart") and v:FindFirstChild("TouchInterest") then part = v; break end end
 
         local function run()
-            plr.Character.Humanoid:EquipTool(tool)
+            local clock = os.time(); plr.Character.Humanoid:EquipTool(tool)
 
             repeat
-                if not getPlayer.Character or not getPlayer.Character.Humanoid or getPlayer.Character.Humanoid.Health <= 0 then
+                if not getPlayer.Character or not getPlayer.Character.Humanoid or getPlayer.Character.Humanoid.Health <= 0 or timer(clock, 5, plr.Character.Humanoid.Health <= 0, function() warn("Task-Failed!") end) then
                     break
                 else
                     plr.Character:SetPrimaryPartCFrame(part.CFrame * CFrame.new(0, 5, 0) * CFrame.Angles(-1.5, 0, 0))
