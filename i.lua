45418e729ffb09c05b8fb8a1ec28cdb405850739
11I1 <kaxcontent@gmail.com>
2024-11-26 17:51:15 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -141,7 +141,7 @@ insertCommand("svoid", function(getPlayer)
 
             repeat task.wait()
                 if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
-                for i, v in next, tool:GetChildren() do if v:IsA("BasePart") then firetouchinterest(getPlayer.Character.PrimaryPart, v.Handle, 0) end end
+                for i, v in next, tool:GetChildren() do if v:IsA("BasePart") then firetouchinterest(getPlayer.Character.PrimaryPart, v, 0) end end
                 if tool.Parent == plr.Character and tool.Parent ~= getPlayer.Character then tool.Parent = workspace end
             until plr.Character.Humanoid.Health <= 0
         end spawn(function() pcall(run, os.time()) end)
