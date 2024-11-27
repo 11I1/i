37cfc930146cc9211530f2e88dbc69cfb593273d
11I1 <kaxcontent@gmail.com>
2024-11-27 00:43:58 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -136,16 +136,17 @@ insertCommand("svoid", function(getPlayer)
     local tool, wand = plr.Backpack["Stroller"] or plr.Character["Stroller"], plr.Backpack["Fairy Wand"] or plr.Character["Fairy Wand"]
 
     local function run(clock)
-        tool.Parent, wand.Parent = plr.Character, plr.Character
+        tool.Parent = plr.Character
 
         repeat task.wait()
             if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
-            tool.Parent, wand.Parent = workspace, workspace
-            firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0, task.wait(), firetouchinterest(getPlayer.Character.PrimaryPart, wand.Handle, 0))
-        until plr.Character.Humanoid.Health <= 0
-    end; task.spawn(function() pcall(run, os.time()) end)
+            if tool.Parent == plr.Character then tool.Parent = workspace end
+            firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0)
+        until tool.Parent == getPlayer.Character
 
-    plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight + 25, 0))
+        wand.Parent, wand.Parent = plr.Character, workspace
+        firetouchinterest(getPlayer.Character.PrimaryPart, wand.Handle, 0)
+    end; pcall(run, os.time())
 
     local clock = os.time()
     repeat task.wait(); if (os.time() - clock) >= 5 then plr.Character.Humanoid:ChangeState(15); return end until plr.Character.Humanoid.Health <= 0 or getPlayer.Character.Humanoid.Health <= 0
