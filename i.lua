0c33380fcca38cfccc262fea43f91002eef9f65b
11I1 <kaxcontent@gmail.com>
2024-11-27 01:15:53 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -142,7 +142,7 @@ insertCommand("svoid", function(getPlayer)
             if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
             if tool.Parent == plr.Character then tool.Parent = workspace end
             firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0)
-        until tool.Parent = getPlayer.Character
+        until tool.Parent == getPlayer.Character
     end; pcall(run, os.time())
 
     wand.Parent = workspace
