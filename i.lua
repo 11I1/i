a11c842f365ccd79eaaaff0a824899a1c55a78fb
11I1 <kaxcontent@gmail.com>
2024-11-27 13:47:26 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -144,10 +144,9 @@ insertCommand("svoid", function(getPlayer)
     firetouchinterest(getPlayer.Character.PrimaryPart, wand.Handle, 0)
     ]]
 
-    tool.Parent, wand.Parent = plr.Character, plr.Character; wait(1/2)
-    wand.Parent = workspace; wait(1/2)
     plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight, 0)); wait(1/2)
-    tool.Parent = workspace; wait(1/2)
+    tool.Parent, wand.Parent = plr.Character, plr.Character; wait(1/2)
+    tool.Parent, wand.Parent = workspace, workspace; wait(1/2)
     firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0)
     firetouchinterest(getPlayer.Character.PrimaryPart, wand.Handle, 0)
 
