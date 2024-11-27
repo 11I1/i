bee2ebf76dc8111f9cd2e99b0986f5b8172b6753
11I1 <kaxcontent@gmail.com>
2024-11-27 00:47:53 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -138,11 +138,13 @@ insertCommand("svoid", function(getPlayer)
     local function run(clock)
         tool.Parent = plr.Character
 
-        repeat task.wait()
-            if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
-            if tool.Parent == plr.Character then tool.Parent = workspace end
-            firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0)
-        until tool.Parent == getPlayer.Character
+        spawn(function()
+            repeat task.wait()
+                if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
+                if tool.Parent == plr.Character then tool.Parent = workspace end
+                firetouchinterest(getPlayer.Character.PrimaryPart, tool.Handle, 0, task.wait(), firetouchinterest(getPlayer.Character.PrimaryPart, wand.Handle, 0))
+            until wand.Parent = getPlayer.Character
+        end)
 
         wand.Parent, wand.Parent = plr.Character, workspace
         firetouchinterest(getPlayer.Character.PrimaryPart, wand.Handle, 0)
