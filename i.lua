37df623967276282a7310e78cf750399a2335023
11I1 <kaxcontent@gmail.com>
2024-11-26 18:10:09 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -137,13 +137,14 @@ insertCommand("svoid", function(getPlayer)
         local tool = plr.Backpack["Stroller"] or plr.Character["Stroller"]
 
         local function run(clock)
-            tool.Parent, tool.Parent = plr.Character, workspace
+            tool.Parent = plr.Character
+            tool.Parent = workspace
 
             repeat task.wait()
                 if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
                 plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight + 25, 0) * CFrame.Angles(-1.5, 0, 0))
-                for i, v in next, tool:GetChildren() do if v:IsA("BasePart") and v.Name ~= "Handle" then firetouchinterest(getPlayer.Character.PrimaryPart, v, 0) end end
-                plr.Character.HumanoidRootPart:BreakJoints()
+                spawn(function() for i, v in next, tool:GetChildren() do if v:IsA("BasePart") and v.Name ~= "Handle" then firetouchinterest(getPlayer.Character.PrimaryPart, v, 0) end end end)
+                if getPlayer.Character.HumanoidRootPart:FindFirstChildOfClass("Weld") then plr.Character.HumanoidRootPart:BreakJoints() end
             until plr.Character.Humanoid.Health <= 0
         end spawn(function() pcall(run, os.time()) end)
 
