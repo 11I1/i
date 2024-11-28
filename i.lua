02aa7c48edceed8aaa7cb547beed034cfdbcc14b
11I1 <kaxcontent@gmail.com>
2024-11-28 16:36:07 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -135,7 +135,7 @@ insertCommand("svoid", function(getPlayer)
     plr.Character.Humanoid:UnequipTools()
     local tool, wand = plr.Backpack["Stroller"] or plr.Character["Stroller"], plr.Backpack["Fairy Wand"] or plr.Character["Fairy Wand"]
 
-    plr.Character:PivotTo(plr.Character:GetModelCFrame() * CFrame.new(0, -500, 0)); wait()
+    plr.Character:PivotTo(plr.Character:GetModelCFrame() * CFrame.new(0, -500, 0)); wait(1/8)
     local clock = os.time()
 
     tool.Parent, wand.Parent = plr.Character, plr.Character
@@ -147,12 +147,8 @@ insertCommand("svoid", function(getPlayer)
         if tool.Parent == plr.Character then tool.Parent = workspace end
     until plr.Character.Humanoid.Health <= 0 or tool.Parent == getPlayer.Character
 
-    plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight, 0)); wait(1/2)
-
-    repeat task.wait()
-        firetouchinterest(wand.Handle, getPlayer.Character.PrimaryPart, 0)
-    until not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0
-
+    plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight, 0)); task.wait(3/4)
+    firetouchinterest(wand.Handle, getPlayer.Character.PrimaryPart, 0)
     plr.Character.Humanoid:ChangeState(15)
 end)
 
