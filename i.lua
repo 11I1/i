3cde42b30bed6c8fa68f795b5224216ec7ee891f
11I1 <kaxcontent@gmail.com>
2024-11-28 13:57:52 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -143,7 +143,16 @@ insertCommand("svoid", function(getPlayer)
         if tool.Parent == plr.Character then tool.Parent = workspace end
     until plr.Character.Humanoid.Health <= 0 or tool.Parent == getPlayer.Character
 
-     plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight, 0))
+    plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight, 0))
+
+    plr.Character.Humanoid:EquipTool(wand)
+    repeat task.wait()
+        if not getPlayer.Character or not getPlayer.Character:FindFirstChildOfClass("Humanoid") or getPlayer.Character.Humanoid.Health <= 0 or (os.time() - clock) >= 5 then break end
+        firetouchinterest(wand.Handle, getPlayer.Character.PrimaryPart, 0)
+        if wand.Parent == plr.Character then wand.Parent = workspace end
+    until plr.Character.Humanoid.Health <= 0 or wand.Parent == getPlayer.Character
+
+    plr.Character.Humanoid:ChangeState(15)
 
     clock = os.time()
     repeat task.wait(); if (os.time() - clock) >= 5 then plr.Character.Humanoid:ChangeState(15); return end until plr.Character.Humanoid.Health <= 0 or getPlayer.Character.Humanoid.Health <= 0
