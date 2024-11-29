eb5c1a8098b6268c377b1a8291d11efe2d89bdc4
11I1 <kaxcontent@gmail.com>
2024-11-28 22:14:40 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -135,7 +135,7 @@ insertCommand("svoid", function(getPlayer)
     plr.Character.Humanoid:UnequipTools()
     local tool, wand = plr.Backpack["Stroller"] or plr.Character["Stroller"], plr.Backpack["Fairy Wand"] or plr.Character["Fairy Wand"]
 
-    plr.Character:PivotTo(plr.Character:GetModelCFrame() * CFrame.new(0, -480, 0)); task.wait(1/2)
+    plr.Character:PivotTo(plr.Character:GetModelCFrame() * CFrame.new(0, -400, 0)); task.wait(1/2)
     local clock = os.time()
 
     tool.Parent, wand.Parent = plr.Character, plr.Character
