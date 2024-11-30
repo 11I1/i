5ca0503111e2d03bab8e934a902a8024ea31dba3
11I1 <kaxcontent@gmail.com>
2024-11-30 17:51:34 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -133,11 +133,10 @@ insertCommand("svoid", function(getPlayer)
     plr.Character.Humanoid:UnequipTools()
     local tool = plr.Backpack["Stroller"] or plr.Character["Stroller"]
 
-    plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight + 2.5, 0)); wait(1/8)
+    plr.Character:PivotTo(CFrame.new(0, workspace.FallenPartsDestroyHeight + 5, 0)); wait(1/8)
 
     tool.Parent, tool.Parent = plr.Character, workspace
     firetouchinterest(tool.Handle, getPlayer.Character.PrimaryPart, 0)
-    plr.Character.Humanoid:ChangeState(15)
 end)
 
 insertCommand("lskill", function(getPlayer)
