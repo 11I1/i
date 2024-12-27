12cf5f4d2b1411e8b9456b1aab628975a35d76c5
11I1 <kaxcontent@gmail.com>
2024-12-27 17:27:40 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -1,9 +1,1 @@
-local ChatService = game:GetService("ChatService")
-local targetPlayer = game.Players:FindFirstChild("5qea") -- replace with the desired username
-
-local message = ChatService.CreateChatMessage()
-message.Message = "Hello, this is a private message!"
-message.Channel = "Private"
-message.Recipient = targetPlayer
-
-ChatService:SendChatMessage(message)
+game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character, "/w 5qea Hello there!")
