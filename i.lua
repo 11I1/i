d3da0563b073098b6ba26ef8ae5ba29daeae79be
11I1 <kaxcontent@gmail.com>
2024-12-27 17:37:46 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -1,1 +1,12 @@
-game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character, "/w 5qea Hello there!")
+local ChatEvents = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents")
+local SayMessageRequest = ChatEvents:WaitForChild("SayMessageRequest")
+
+-- Whispering a private message
+local targetUsername = "5qea" -- Replace with the username of the recipient
+local message = "This is a private message!"
+
+-- Format the whisper message
+local whisperMessage = string.format("%s", message)
+
+-- Attempt to send the message using the "Whisper" channel
+SayMessageRequest:FireServer(whisperMessage, "Whisper", targetUsername)
