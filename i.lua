d8288f27ce60b490e3192d776acc20bfd51957bc
11I1 <kaxcontent@gmail.com>
2024-12-27 21:47:28 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -4,9 +4,9 @@ local workspace, plrs, rstorage, startergui, rservice = workspace, game.Players,
 local id, plr, dcsce = game.PlaceId, plrs.LocalPlayer, rstorage:FindFirstChild("DefaultChatSystemChatEvents")
 
 local utilities, signals, loops, ranking, ids = {["DevConsoleVisible"] = false}, {}, {}, {[plr] = 1}, {1662219031}
-local findID, Commands = ids[id], ""
+local findID, Commands = ids[id]
 
-task.defer(function() for i, v in next, api.cmds do Commands += tostring(i) + " " end warn(Commands) end)
+task.defer(function() for i, v in next, api.cmds do Commands = Commands.." "..i end warn(Commands) end)
 
 if findID then
     for i, v in next, plrs:GetPlayers() do if v ~= plr then ranking[v] = ranking[plr] + 1 end end
