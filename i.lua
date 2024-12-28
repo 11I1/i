8702f96529669d4c0e09d43e75472bef0fdf2c3d
11I1 <kaxcontent@gmail.com>
2024-12-27 18:45:46 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -6,7 +6,7 @@ local id, plr, dcsce = game.PlaceId, plrs.LocalPlayer, rstorage:FindFirstChild("
 local utilities, signals, loops, ranking, ids = {["DevConsoleVisible"] = false}, {}, {}, {[plr] = 1}, {1662219031}
 local findID, Commands = ids[id]
 
-task.defer(function() for i, v in next, api.cmds do Commands += i + " " end warn(Commands) end)
+task.defer(function() for i, v in next, api.cmds do Commands += tostring(i) + " " end warn(Commands) end)
 
 if findID then
     for i, v in next, plrs:GetPlayers() do if v ~= plr then ranking[v] = ranking[plr] + 1 end end
