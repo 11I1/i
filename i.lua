3dc24f383e4c1c8c09c4125e88ea15b127bd63f4
11I1 <kaxcontent@gmail.com>
2024-12-30 11:15:36 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -129,8 +129,6 @@ insertCommand("kill", function(player)
 
     local obj, objt = plr.Character, player.Character
 
-    task.defer(function() timer(5, function() obj.Humanoid:ChangeState(15) end) end)
-
     local tool, toolParts, killPart = plr.Backpack.Stroller or obj.Stroller, {}
     for i, v in next, tool:GetChildren() do if v:IsA("BasePart") and v:FindFirstChildOfClass("TouchTransmitter") then toolParts[#toolParts + 1] = v end end
     for i, v in next, workspace["Police Station"]:GetChildren() do if v:IsA("BasePart") and v:FindFirstChildOfClass("TouchTransmitter") then killPart = v; break end end
@@ -144,7 +142,6 @@ insertCommand("kill", function(player)
     for i, v in next, toolParts do firetouchinterest(v, objt.PrimaryPart, 0, task.wait(), firetouchinterest(v, objt.PrimaryPart, 1)) end
     wait(2)
     firetouchinterest(objt.HumanoidRootPart, killPart, 0, task.wait(), firetouchinterest(objt.HumanoidRootPart, killPart, 1))
-    obj.Humanoid:UnequipTools()
 end)
 
 insertCommand("as", function(getPlayer)
