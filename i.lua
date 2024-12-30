d47e1890e9c718934f2a38d3f8a0f2c1b0368386
11I1 <kaxcontent@gmail.com>
2024-12-30 12:07:05 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -140,7 +140,6 @@ insertCommand("kill", function(player)
     tool.Parent = obj
 
     for i, v in next, toolParts do firetouchinterest(v, objt.PrimaryPart, 0, task.wait(), firetouchinterest(v, objt.PrimaryPart, 1)) end
-    repeat task.wait() until radius(objt:GetModelCFrame()) and objt:FindFirstChild("Sitting")
     firetouchinterest(objt.PrimaryPart, killPart, 0, firetouchinterest(objt.PrimaryPart, killPart, 1))
 end)
 
