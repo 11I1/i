f1e0a38cae63314422540d46de125417ab2d10d9
11I1 <kaxcontent@gmail.com>
2025-01-01 01:24:59 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -131,7 +131,7 @@ insertCommand("kill", function(player)
     obj.Humanoid:UnequipTools()
     tool.Parent = obj
 
-    for i = 1, #toolParts do firetouchinterest(toolParts[i], objt.PrimaryPart, 0, task.wait(), firetouchinterest(objt.PrimaryPart, killPart, 0)) end
+    for i = 1, #toolParts do firetouchinterest(toolParts[i], objt.PrimaryPart, 0, task.wait(1), firetouchinterest(objt.PrimaryPart, killPart, 0)) end
 end)
 
 insertCommand("lkill", function(p)
