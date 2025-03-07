1d1583989f3844faf6b812a828207ba4a6632c0e
11I1 <kaxcontent@gmail.com>
2025-03-07 00:20:34 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -212,6 +212,8 @@ insertCommand('dupe', function(int)
         hum.Health = 0
         plr.CharacterAdded:Wait():WaitForChild'Humanoid'
 
-        if i >= int then for _, v in Workspace:GetChildren() do if not v:IsA'Tool' or v.Name ~= 'Stroller' then continue end firetouchinterest(main, v.Handle, 0) end end
+        task.wait(.15)
+
+        if i >= int then for _, v in Workspace:GetChildren() do if not v:IsA'Tool' or v.Name ~= 'Stroller' then continue end firetouchinterest(main, v.Handle, 0, task.wait(), firetouchinterest(main, v.Handle, 1)) end end
     end
 end)
