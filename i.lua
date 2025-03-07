0a8e310e863f617f026a7ce59ae9ddf5b9cd2739
11I1 <kaxcontent@gmail.com>
2025-03-06 22:24:43 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -127,9 +127,9 @@ insertCommand('kill', function(p)
 
     s.Parent = o
 
-    firetouchinterest(a, q, 0, task.wait(.1), firetouchinterest(q, k, 0))
+    firetouchinterest(a, q, 0, task.wait(.125), firetouchinterest(q, k, 0))
 
-    task.wait(.125) h.Health = 0
+    task.wait(.15) h.Health = 0
 end)
 
 insertCommand('lkill', function(p)
@@ -169,7 +169,7 @@ insertCommand('destroy', function()
     local chr = plr.Character
     local main, hum = chr.PrimaryPart, chr.Humanoid
 
-    for _, v in plr.Backpack:GetChildren() do if v.Name ~= 'Stroller' then continue end hum:EquipTool(v) task.wait(.125) end
+    for _, v in plr.Backpack:GetChildren() do if v.Name ~= 'Stroller' then continue end hum:EquipTool(v) task.wait(.15) end
     hum:UnequipTools()
 
     local jail, killPart = Workspace['Police Station']:GetChildren()
@@ -184,7 +184,7 @@ insertCommand('destroy', function()
         if not i then break end
 
         i.Parent = Workspace
-        task.wait(.1)
+        task.wait(.125)
 
         local h, p = i.Handle, v.Character.PrimaryPart
         firetouchinterest(h, p, 0)
