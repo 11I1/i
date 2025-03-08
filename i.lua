f8b8156c3eda1c9eb734bcac904a5ddf38c7a5ba
11I1 <kaxcontent@gmail.com>
2025-03-07 22:44:14 -0600

    Update i.lua

--- i.lua
+++ i.lua
@@ -221,7 +221,7 @@ insertCommand('dupe', function(int)
             c = plr.Character
             local p = c.PrimaryPart
 
-            for _, v in Workspace:GetChildren() do if not v:IsA'Tool' or v.Name ~= 'Stroller' then continue end firetouchinterest(p, v.Handle, 0) end
+            for _, v in next, Workspace:GetChildren() do if not v:IsA'Tool' or v.Name ~= 'Stroller' then continue end firetouchinterest(p, v.Handle, 0) end
         end
     end
 end)
