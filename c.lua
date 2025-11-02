2dad23d898f01a231014a2a1794108c9c3cca96e
11I1 <kaxcontent@gmail.com>
2025-11-02 14:38:33 -0600

    Create c.lua

---
+++ c.lua
@@ -0,0 +1,12 @@
+local G, T, R = getgenv, typeof, rawset
+local S = G().Signals
+
+local AddSignal = setmetatable(S, {
+    __newindex = function(t, k, v)
+        if T(v) == "RBXScriptConnection" then
+            R(t, k, v)
+        else
+            R(t, k, nil)
+        end
+    end
+})
