c93ad9a58dac13c90795aadd66ab494c04b34cbc
11I1 <kaxcontent@gmail.com>
2025-11-05 20:06:52 -0600

    Update d.lua

--- d.lua
+++ d.lua
@@ -6,13 +6,13 @@ local PP, MB2 = L.Character.PrimaryPart, Enum.UserInputType.MouseButton2
 
 local function Press(K)
 	V:SendKeyEvent(true, K, false, game)
-	task.wait(0.01)
+	task.wait(.015)
 	V:SendKeyEvent(false, K, false, game)
 end
 
 local DL, DR = function() Press("A") G:Dive() end, function() Press("D") G:Dive() end
 
-AddSignal["b"] = UI.InputBegan:Connect(function(i, g)
+AddSignal["a"] = UI.InputBegan:Connect(function(i, g)
 	if g or i.UserInputType ~= MB2 then return end
 	if PP.CFrame:PointToObjectSpace(F.Position).X > 0 then DR() else DL() end
 end)
